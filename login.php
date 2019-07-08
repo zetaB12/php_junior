06e37be49295c013f2a9922c24364584f86650e2
Deyvi <deyvizavaleta@gmail.com>
2019-07-08 11:15:26 -0500

    se agrego un index.js y se quito un parrafo del login.php

--- login.php
+++ login.php
@@ -16,11 +16,6 @@
             <div class="col_1_of_login span_1_of_login">
                 <h4 class="title">New Customers</h4>
                 <h5 class="sub_title">Register Account</h5>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                    laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                    ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
-                    dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla
-                    facilisis at vero eros et accumsan</p>
                 <div class="button1">
                     <a href="register.php"><input type="submit" name="Submit" value="Continue"></a>
                 </div>
